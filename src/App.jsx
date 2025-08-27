f93ed77d4d2e50acf9693bb6c1683a18786ab293
Aabhas Nama <aabhasa9@gmail.com>
2025-08-27 19:27:39 +0530

    hero and navbar sections done

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import LiquidChrome from './components/Hero/LiquidChrome';
+import Navbar from './components/Navbar';
+import Home from './Pages/Home';
+import MainRoutes from './Routes/MainRoutes';
+
+
+
+const App = () => {
+  return (
+
+
+    <div className="w-full h-screen relative">
+
+  
+    <Navbar/>
+    <MainRoutes/>
+
+
+    </div>
+
+
+  )
+}
+
+export default App
+
+
+
+
